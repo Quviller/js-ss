0ebddc27edb691895770c9c38518cad9f024cb49
Roman Didukh <didukhroman@gmail.com>
2016-08-31 12:50:28 +0000

    module1 complete

---
+++ module-1/m01task01.js
@@ -0,0 +1,11 @@
+﻿// Task01 - Write a code that returns any string value.
+// For example, next code return a boolean value: 
+// return false;
+
+function task01(a) {
+    a='hey, dude';
+    return a;
+}
+
+console.log(task01());
+
