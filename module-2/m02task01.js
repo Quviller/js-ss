7e9046286f3a6a29c3a9d694918ef6d008b551bf
Roman Didukh <didukhroman@gmail.com>
2016-08-31 13:05:58 +0000

    module2 complete

---
+++ module-2/m02task01.js
@@ -0,0 +1,8 @@
+﻿// Task01 - create a function with name task01
+// Function should return any string value
+
+function task01() {
+    return 'hello,dude';
+}
+
+console.log(task01());
---
+++ module-2/m02task02.js
@@ -0,0 +1,10 @@
+﻿// Task02 - create function with name task02
+// Function should pass two parameters 
+// and return number - sum of input parameters
+// Note: strings should be casted to Number type
+
+function task02(a,b) {
+    return Number(a)+Number(b);
+}
+
+console.log(task02("12",2));
