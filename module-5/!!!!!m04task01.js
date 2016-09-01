2d93055669927e33dd1cf4f9ff307f525bf6f3b3
Roman Didukh <didukhroman@gmail.com>
2016-09-01 11:10:27 +0000

    module5 complete

---
+++ module-5/!!!!!m04task01.js
@@ -0,0 +1,8 @@
+﻿// Task01 - Create a simple function that will be used as event handler. 
+// function should cancel event bubbling
+
+function task01(e) {
+    alert('It\'s a joke');
+    e.stopPropagation();
+}
+  
