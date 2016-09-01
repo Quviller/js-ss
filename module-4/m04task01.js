73b878029eabfd44f0e16050ca5910ef65384c3c
Roman Didukh <didukhroman@gmail.com>
2016-09-01 10:07:58 +0000

    module4 complete

---
+++ module-4/m04task01.js
@@ -0,0 +1,18 @@
+﻿// Task01 - Create a simple object cat and return it from function.
+//
+// Object should have next attributes:
+// color with value "white"
+// age with value 3
+// name - Lucky
+
+function Cat(name, age, color) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    return this;
+    
+}
+
+var Lucky = new Cat ('Lucky', 3,'white');
+
+console.log(Lucky);
---
+++ module-4/m04task03.js
@@ -0,0 +1,28 @@
+﻿// Task03 - Update your constructor Cat from task 2
+// Constructor should add to objects method action()
+// After calling method action return string with next format:
+// color + " cat " + name + " run!"
+
+// For example if cat created with parameters "white" and "Lucky"
+// it action() should be "white cat Lucky run!"
+
+// P.S
+// Remember about this
+
+function task03(color, name) {
+    return new Cat(color, name); // don't change this code
+}
+
+// TODO: Define your constructor here 
+
+function Cat(color, name) {
+    this.color=color;
+    this.name=name;
+    this.run= function (){
+        console.log(color + " cat " + name + " run!");
+    };
+}
+
+var Lucky = task03('white', "lucky");
+
+Lucky.run();
