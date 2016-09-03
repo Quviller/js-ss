237e14267ab837d82857f2b69d61a5e17373027f
Roman Didukh <didukhroman@gmail.com>
2016-09-03 09:25:08 +0000

    module7 complete

---
+++ module-7/m07task02.js
@@ -0,0 +1,14 @@
+﻿// Task02
+// Using JavaScript and DOM hide hide second and third paragraphs 
+// of target HTML document, all other elements should be visible
+
+    function m07task02() {
+      var x = document.getElementsByClassName('test');
+      var i;
+      for (i=0; i<x.length;i++) {
+          x[i].setAttribute('hidden',"hidden");
+      }
+      
+    }
+    
+    m07task02();
---
+++ module-7/m07task03.js
@@ -0,0 +1,9 @@
+﻿// Task03 
+// Using JavaScript and DOM create click event handler for Button that 
+// replaces content of a paragraph 4 with a number 42
+
+function m07task03() {
+  document.getElementById('last').innerHTML = '42';
+}
+
+document.getElementById('testButton').addEventListener('click', m07task03,false);
